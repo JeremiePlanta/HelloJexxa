4c0526f419c4d8b53e34b9614fec0c80a8c8441b
PJeremie7 <jeremie.planta@stahl-holding-saar.de>
2021-10-01 14:38:59 +0200

    Created HelloJexxa class v1.0 and completed the pom.xml

---
+++ src/main/java/org/example/HelloJexxa.java
@@ -0,0 +1,19 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public final class HelloJexxa
+{
+    public static String greetings()
+    {
+        return "Hello Jexxa";
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(greetings());
+    }
+}
+
